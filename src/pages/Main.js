713ff833eaa274c9d35695ed52fa08d1580ed965
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-02-21 23:14:12 +0900

    UI edit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import bae from '../bae.gif'
+
 
 export const Main = () => {
   return (
-    <div>Welcome from my awesome app.</div>
+    <>
+    <br/>
+    <div>Honey koe chit tl 	<span style={{color:"red"}}>&#10084;</span></div>
+    <img style={{margin:"45px",width:"450px",height:"600px"}} src={bae}></img>
+    </>
   )
 }
