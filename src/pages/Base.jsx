6a10bd692e0278828d2107833c98a9b0d4c43200
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-03-02 22:48:05 +0900

    adding tailwind

--- src/pages/Base.jsx
+++ src/pages/Base.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Menu from './components/Menu'
 
 export default function Base() {
   return (
     <>
-    <div>Welcome to my site.</div>
-    <p>This will be my testing ground on different technologies.</p>
+    <div className="text-lg font-bold">Welcome to my site</div>
+    <p>This will be my testing ground on different technologies</p>
+    <Menu></Menu>
     </>
   )
 }
