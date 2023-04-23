84b579ef08030351b8fd434bbadb831e8f892d39
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-04-23 21:08:13 +0900

    changing project page UI

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,125 +1,58 @@
-import { useState } from 'react'
-import { Tab } from "@headlessui/react"
+import { Card, CardHeader,Flex, Box, IconButton, Avatar, Heading, CardBody, CardFooter, Button, Image, Text } from '@chakra-ui/react'
+import { AiOutlineLike, AiOutlineDislike  } from "react-icons/ai"
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
 
 // Must do better with a history kind of like table
 export const Projects = () => {
-    let [categories] = useState({
-        Recent: [
-          {
-            id: 1,
-            title: 'Does drinking coffee make you smarter?',
-            date: '5h ago',
-            commentCount: 5,
-            shareCount: 2,
-          },
-          {
-            id: 2,
-            title: "So you've bought coffee... now what?",
-            date: '2h ago',
-            commentCount: 3,
-            shareCount: 2,
-          },
-        ],
-        Popular: [
-          {
-            id: 1,
-            title: 'Is tech making coffee better or worse?',
-            date: 'Jan 7',
-            commentCount: 29,
-            shareCount: 16,
-          },
-          {
-            id: 2,
-            title: 'The most innovative things happening in coffee',
-            date: 'Mar 19',
-            commentCount: 24,
-            shareCount: 12,
-          },
-        ],
-        Trending: [
-          {
-            id: 1,
-            title: 'Ask Me Anything: 10 answers to your questions about coffee',
-            date: '2d ago',
-            commentCount: 9,
-            shareCount: 5,
-          },
-          {
-            id: 2,
-            title: "The worst advice we've ever heard about coffee",
-            date: '4d ago',
-            commentCount: 1,
-            shareCount: 2,
-          },
-        ],
-      })
+
+      // the data should really come from the backend
     
       return (
         <div className="w-10/12 m-auto px-2 py-16 sm:px-0">
-          <Tab.Group>
-            <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-              {Object.keys(categories).map((category) => (
-                <Tab
-                  key={category}
-                  className={({ selected }) =>
-                    classNames(
-                      'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
-                      'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                      selected
-                        ? 'bg-white shadow'
-                        : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
-                    )
-                  }
-                >
-                  {category}
-                </Tab>
-              ))}
-            </Tab.List>
-            <Tab.Panels className="mt-2">
-              {Object.values(categories).map((posts, idx) => (
-                <Tab.Panel
-                  key={idx}
-                  className={classNames(
-                    'rounded-xl bg-white p-3',
-                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-                  )}
-                >
-                  <ul>
-                    {posts.map((post) => (
-                      <li
-                        key={post.id}
-                        className="relative rounded-md p-3 hover:bg-gray-100"
-                      >
-                        <h3 className="text-sm font-medium leading-5">
-                          {post.title}
-                        </h3>
-    
-                        <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                          <li>{post.date}</li>
-                          <li>&middot;</li>
-                          <li>{post.commentCount} comments</li>
-                          <li>&middot;</li>
-                          <li>{post.shareCount} shares</li>
-                        </ul>
-    
-                        <a
-                          href="#"
-                          className={classNames(
-                            'absolute inset-0 rounded-md',
-                            'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
-                          )}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                </Tab.Panel>
-              ))}
-            </Tab.Panels>
-          </Tab.Group>
+          {/* For loop */}
+            <Card maxW='md'>
+              <CardHeader>
+                <Flex spacing='4'>
+                  <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                    <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+
+                    <Box>
+                      <Heading size='sm'>Zaw Ye Htet Naing</Heading>
+                      <Text>2000-2001 ,1 year</Text>
+                    </Box>
+                  </Flex>
+                </Flex>
+              </CardHeader>
+              <CardBody style={{display:"flex",flexDirection:"column",textAlign:"left"}}>
+                <Text>
+                  With Chakra UI
+                </Text>
+                <Text>Hello</Text>
+              </CardBody>
+              <Image
+                objectFit='cover'
+                src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                alt='Chakra UI'
+                style={{width:'98%', margin:"auto"}}
+              />
+
+              <CardFooter
+                justify='space-between'
+                flexWrap='wrap'
+                sx={{
+                  '& > button': {
+                    minW: '136px',
+                  },
+                }}
+              >
+                <Button flex='1' variant='ghost' leftIcon={<AiOutlineLike/>}>
+                  Like
+                </Button>
+                <Button flex='1' variant='ghost' leftIcon={<AiOutlineDislike/>}>
+                  Dislike
+                </Button>
+              </CardFooter>
+            </Card>
         </div>
       )
 }
