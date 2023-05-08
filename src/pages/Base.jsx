db6ec77c6bd685f7c91c3d7ed6ea2d3dd14ed86b
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-05-08 22:08:27 +0900

    adding and managing fonts

--- src/pages/Base.jsx
+++ src/pages/Base.jsx
@@ -6,6 +6,13 @@ export default function Base() {
   return (
     <>
     <Menu></Menu>
+    <div>
+      Font test <br />
+      <p className='font-neuFace'>Neutral Face</p>
+      <p className='font-sans'>Sans</p>
+      <p className='font-serif'>Serif</p>
+      <p className='font-mono'>Mono</p>
+    </div>
     <Outlet></Outlet>
     </>
   )
