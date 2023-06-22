9db8ead17e917ec49c4bbf2172046946298ac88d
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-06-22 22:18:24 +0900

    projects UI update

--- src/pages/parts/Projects.jsx
+++ src/pages/parts/Projects.jsx
@@ -4,9 +4,21 @@ import React from 'react'
 
 export default function Projects() {
 
+  const mock_data = [
+    {
+      projectName:"Hello"
+    },
+    {
+      projectName:"One or Two"
+    }
+  ]
+
   return (
-    <div className="bg-black-300 w-[300px]">
+    <div className="h-fit flex flex-col items-center space-y-8">
             <a href='https://chimerical-cendol-b65002.netlify.app/'>Sample Here....</a>
+            {mock_data.map(d => <div className='border-solid border-black border-2 w-[250px] h-[280px] rounded-lg '>
+            {d.projectName}
+            </div>)}
     </div>
   )
 }
