4a5065af8692ca12c2f6153eb9011e23459dfda7
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-07-24 22:07:14 +0900

    some size adjustments and ideas

--- src/pages/parts/Aboutme.jsx
+++ src/pages/parts/Aboutme.jsx
@@ -12,17 +12,19 @@ export default function Aboutme() {
         <div className="w-4/6 md:w-2/6">
           <img src={profileImg} alt="My Image" className="w-[170px] h-[240px] md:static md:h-full md:w-full" />
         </div>
-        <div className="w-2/4 md:w-4/6 font-boola">
+        <div className="w-2/4 md:w-4/6 font-boola text-xs md:text-base">
           <p className="text-left font-sans">Welcome to my portfolio website!<br /><br />I'm Zaw Ye Htet Naing, a software engineer. Here, you will find a glimpse of my 
           professional world.<br /> <br />
-          With a mind for continuous learning, I am always looking to fill this place with more awesome. Explore my projects and let's connect to create something awesome together. 
+          With a mind for continuous learning, I am always looking to fill this place with more awesome projects. Explore my projects and let's connect to create something awesome together. 
           Hope you enjoy your stay here.
           <br /><br />
           Best Regards, <br />
           <i>Zaw</i>
 
           </p>
+          <div className="mt-6 flex justify-around">CV, Linkedin</div>
         </div>
+        
     </div>
     </div>
   )
