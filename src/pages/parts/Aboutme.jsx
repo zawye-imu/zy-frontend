37406b0f013642f7f6c1e23443c38b3d322b8329
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-07-27 22:07:22 +0900

    add connections

--- src/pages/parts/Aboutme.jsx
+++ src/pages/parts/Aboutme.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import profileImg from '../../profile_1.png'
 import LinkedInImg from '../../images/linkedin.png';
-
+import githubImg from '../../images/github.png';
+import githubImg2 from '../../images/github2.jpg';
+import cvImg from '../../images/cv.png';
 
 export default function Aboutme() {
 
@@ -23,11 +25,19 @@ export default function Aboutme() {
           <i>Zaw</i>
 
           </p>
-          <div className="mt-6 flex flex-start justify-start">
-          <img alt='cv' className='mr-4'></img> 
-          <img src={LinkedInImg} className='w-6 h-6 mr-4 hover:cursor-pointer' onClick={()=>{
+          <div className="mt-6 flex flex-start justify-start items-center">
+            
+          <img src={cvImg} alt='cv' className='w-8 h-8 mr-8 hover:cursor-pointer' onClick={()=>{
+            window.open("https://2018416e-28a1-4020-be3c-c5deefebfcf5.filesusr.com/ugd/66b817_c1c2480746e647bba2cf248a1bb185b3.pdf","_blank")
+          }}>
+          </img> 
+          <img src={LinkedInImg} className='w-6 h-6 mr-8 hover:cursor-pointer' onClick={()=>{
             window.open("https://www.linkedin.com/in/zawyehtetnaing/","_blank")
-          }}></img></div>
+          }}></img>
+          <img src={githubImg2} className='w-6 h-6 mr-8 hover:cursor-pointer' onClick={()=>{
+            window.open("https://github.com/zawye-imu","_blank")
+          }}></img>
+          </div>
         </div>
         
     </div>
