bdea8c30d7f138af652237c529aa2af2cbe412b4
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2023-09-05 22:10:59 +0900

    adding project informations

--- src/pages/parts/Projects.jsx
+++ src/pages/parts/Projects.jsx
@@ -1,30 +1,47 @@
 import React from 'react'
 import "../styles/projectStyle.css";
+import ERP from "../../images/ERP.png"
 
 export default function Projects() {
 
   const mock_data = [
     {
+      projectName:"Project Management System",
+      img: ""
+    },
+    {
+      projectName:"NLP - emotion detection from twitter posts using ANN",
+      img: ""
+    },
+    {
       projectName:"Customized ERP System",
-      img: "ERP.png"
+      img: ERP
     },
     {
-      projectName:"One or Two",
+      projectName:"Odoo Cloud Development",
+      img: "logo512.png"
+    },
+    {
+      projectName: "Knorr Donation Website",
+      img: "src/images/Knorr.jpg"
+    },
+    {
+      projectName: "POS website using YII",
       img: ""
     },
     {
-      projectName: "The third One",
+      projectName: "Library Management System",
       img: ""
-    }
+    },
     
   ]
 
   return (
     <div>
       <h1 className='font-boola text-2xl my-4'>Projects & Experiences</h1>
-      <div className="mt-16 w-10/12 m-auto h-fit flex flex  -col items-center space-y-8 md:flex-row md:space-x-32 md:space-y-0">
-              {mock_data.map(d => 
-              <div className="projectCard border-solid border-black border-2 w-[250px] h-[280px] rounded-lg bg-[url('/ERP.png')] bg-center bg-cover bg-origin-padding">
+      <div className="mt-16 w-10/12 m-auto flex flex-wrap items-center">
+              {mock_data.map((d,idx) => 
+              <div className={`projectCard border-solid border-black border-2 w-[250px] h-[280px] m-8 rounded-lg bg-center bg-cover bg-origin-padding`  + ` bg-[url('${d.img}')]`}>
               <div className='hiddenText'>{d.projectName}</div>
               </div>
               )}
